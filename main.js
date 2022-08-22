35cbc4c61e987c2b643ad77e87cd49d4ea6e4a05
MarcinSkic <marcinskicoficjal@gmail.com>
2022-08-22 20:28:24 +0200

    Add game restart

--- main.js
+++ main.js
@@ -71,7 +71,6 @@ const gameController = (function(doc){
     }
 
     const setupGame = function(event){
-        console.log(event.target.returnValue);
 
         const firstPlayerUsername = event.target.querySelector('[name="player-0"]').value;
         players.push(playerFactory(doc,firstPlayerUsername,Symbols.Cross));
@@ -92,10 +91,10 @@ const gameController = (function(doc){
         if(didWin){
             winnerUsername = players[currentPlayer].getUsername();
             winnerUsername = winnerUsername === null ? 'AI' : `Player "${winnerUsername}"`;
-            console.log(`${winnerUsername} won!`);
+            displayController.showGameEndDialog(`${winnerUsername} WON!`);
             return;
         } else if(currentTurn === 9){
-            console.log("End of game");
+            displayController.showGameEndDialog("TIE");
             return;
         }
 
@@ -116,6 +115,18 @@ const gameController = (function(doc){
 
     const assignListeners = function(){
         doc.querySelector('.choose-names').addEventListener('close',setupGame);
+        doc.querySelector('.restart').addEventListener('click',resetGame);
+    }
+
+    const resetGame = function(){
+        currentTurn = 0;
+        currentPlayer = 1;
+        players.length = 0;
+
+        gameBoard.clearData();
+        displayController.clearBoard();
+        displayController.fixAIInput();
+        displayController.showGameTypeDialog();
     }
 
     return {initializeGame, prepareNextTurn};
@@ -192,10 +203,16 @@ const gameBoard = (function(){
             }
         }
 
+        
+
         return checkColumn() || checkRow() || checkDiagonals();
     }
 
-    return {isFieldMarked, markField, didWin};
+    const clearData = function(){
+        board = Array.from(Array(3),() => new Array(3));
+    }
+
+    return {isFieldMarked, markField, didWin, clearData};
 })();
 
 const displayController = (function(doc){
@@ -248,8 +265,14 @@ const displayController = (function(doc){
         }
     }
 
+    const clearBoard = function(){
+        doc.querySelectorAll('.field path').forEach(path => path.setAttribute('d',''));
+    }
+
     const setupPlayersUI = function(firstName, secondName){
+        firstPlayer.classList.remove('active');
         firstPlayer.firstChild.textContent = firstName;
+        secondPlayer.classList.add('active');
         secondPlayer.firstChild.textContent = secondName;
     }
 
@@ -260,6 +283,12 @@ const displayController = (function(doc){
 
     const drawSymbol = function(clickedNode, symbol){
         clickedNode.querySelector("path").setAttribute('d',symbolToPath(symbol));
+    }
+
+    const fixAIInput = function(){
+        AIInput = namePlayersDialog.querySelector('input[type="text"]:last-of-type');
+        AIInput.disabled = false;
+        AIInput.value = "";
     }
 
     const stringToNode = function(string){
@@ -277,8 +306,14 @@ const displayController = (function(doc){
         }
     }
 
-    return {showGameTypeDialog,showGameEndDialog,generateBoard,setupPlayersUI,changeActivePlayer,drawSymbol};
+    return {showGameTypeDialog,showGameEndDialog,generateBoard,clearBoard,setupPlayersUI,changeActivePlayer,drawSymbol,fixAIInput};
     
 })(document);
 
 gameController.initializeGame();
+
+/* Angry Robot if I ever implement unbeatable AI
+<svg style="width:24px;height:24px" viewBox="0 0 24 24">
+    <path fill="currentColor" d="M22 14H21C21 10.13 17.87 7 14 7H13V5.73C13.6 5.39 14 4.74 14 4C14 2.9 13.11 2 12 2S10 2.9 10 4C10 4.74 10.4 5.39 11 5.73V7H10C6.13 7 3 10.13 3 14H2C1.45 14 1 14.45 1 15V18C1 18.55 1.45 19 2 19H3V20C3 21.11 3.9 22 5 22H19C20.11 22 21 21.11 21 20V19H22C22.55 19 23 18.55 23 18V15C23 14.45 22.55 14 22 14M7.5 18C6.12 18 5 16.88 5 15.5C5 14.68 5.4 13.96 6 13.5L9.83 16.38C9.5 17.32 8.57 18 7.5 18M16.5 18C15.43 18 14.5 17.32 14.17 16.38L18 13.5C18.6 13.96 19 14.68 19 15.5C19 16.88 17.88 18 16.5 18Z" />
+</svg>
+*/
